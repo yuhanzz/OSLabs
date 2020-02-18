b9d5a14aa72fafc36b1241122a9982e2d6cea74a
Yuhan Zhou <yuhan.zhou98@gmail.com>
2020-02-18 16:43:01 -0500

    complete version 1

--- OSLab1/Tokenizer.cpp
+++ OSLab1/Tokenizer.cpp
@@ -4,8 +4,6 @@
 #include <regex>
 #include "Tokenizer.h"
 
-// Token::Token(int lineNumber, int lineOffset, std::string token) : lineNumber(lineNumber), lineOffset(lineOffset), token(token) {}
-
 Token::Token(int lineNumber, int lineOffset, std::string token)
 {
     this->lineNumber = lineNumber;
@@ -18,18 +16,12 @@ bool Token::isEmpty()
     return token.compare("") == 0;
 }
 
-// Tokenizer::Tokenizer(ifstream& infile) : lineNumber(0), lineOffset(0), infile(infile) {}
-
 Tokenizer::Tokenizer()
 {
     currentLineNumber = 0;
-    // null opinter - base address -> lead to a big number
     currentLine = NULL;
     currentLineLength = 0;
 };
-
-// // can return a boolean value to indicate
-// void Token
 
 bool Tokenizer::getLine()
 {
@@ -90,10 +82,13 @@ int Tokenizer::readAddress()
     {
         throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
     }
-    // 需要处理的异常：地址值超过限制
     try
     {
         int address = std::stoi(token.token);
+        if (address < 0) 
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
         return address;
     }
     catch (std::invalid_argument &e)
@@ -102,7 +97,15 @@ int Tokenizer::readAddress()
     }
     catch (std::out_of_range &e)
     {
-        throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        if (token.token.at(0) == '-')
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
+        else
+        {
+            // if the number is larger than integer, it will be caught and treated as relative address zero in pass2
+            return 10000;
+        }
     }
 }
 
@@ -113,13 +116,15 @@ int Tokenizer::readUseCount()
     {
         throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
     }
-    // 需要处理的异常：地址值超过限制
     try
     {
         int count = std::stoi(token.token);
         if (count > 16)
         {
             throw SyntaxError(TOO_MANY_USE_IN_MODULE, token.lineNumber, token.lineOffset);
+        }
+        if (count < 0) {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
         }
         return count;
     }
@@ -129,7 +134,14 @@ int Tokenizer::readUseCount()
     }
     catch (std::out_of_range &e)
     {
-        throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        if (token.token.at(0) == '-')
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
+        else
+        {
+            throw SyntaxError(TOO_MANY_USE_IN_MODULE, token.lineNumber, token.lineOffset);
+        }
     }
 }
 
@@ -140,10 +152,12 @@ int Tokenizer::readInstCount(int &totalInstCount)
     {
         throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
     }
-    // 需要处理的异常：地址值超过限制
     try
     {
         int count = std::stoi(token.token);
+        if (count < 0) {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
 
         totalInstCount += count;
         if (totalInstCount > 512)
@@ -158,7 +172,14 @@ int Tokenizer::readInstCount(int &totalInstCount)
     }
     catch (std::out_of_range &e)
     {
-        throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        if (token.token.at(0) == '-')
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
+        else
+        {
+            throw SyntaxError(TOO_MANY_INSTR, token.lineNumber, token.lineOffset);
+        }
     }
 }
 
@@ -192,13 +213,6 @@ char Tokenizer::readIEAR()
 int Tokenizer::readOperand()
 {
 
-    // 目前来说operand不该这样处理，因为可能以0开头
-
-    // if exceeds 四位数，pass2处理
-
-    //负数怎么办？？？
-    // check 每一位是不是digit??，也有可能是0开头，必须要是四位数？？？
-
     Token token = getToken();
     if (token.isEmpty())
     {
@@ -207,6 +221,10 @@ int Tokenizer::readOperand()
     try
     {
         int operand = std::stoi(token.token);
+        if (operand < 0) 
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
         return operand;
     }
     catch (std::invalid_argument &e)
@@ -215,19 +233,14 @@ int Tokenizer::readOperand()
     }
     catch (std::out_of_range &e)
     {
-        throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
-    }
-    // catch (std::out_of_range &e)
-    // {
-    //     // if the converted value would fall out of the range of the result type
-    //     // or if the underlying function (std::strtol or std::strtoull) sets errno
-    //     // to ERANGE.
-
-    //     // do not throw
-    // }
-    catch (std::exception &e)
-    {
-        throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        if (token.token.at(0) == '-')
+        {
+            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+        }
+        else
+        {
+            return 10000;
+        }
     }
 }
 
@@ -261,13 +274,16 @@ bool Tokenizer::readDefCount(int &defcount)
     }
     else
     {
-        // do some check !!!
         try
         {
             defcount = std::stoi(token.token);
             if (defcount > 16)
             {
                 throw SyntaxError(TOO_MANY_DEF_IN_MODULE, token.lineNumber, token.lineOffset);
+            }
+            if (defcount < 0) 
+            {
+                throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
             }
         }
         catch (std::invalid_argument &e)
@@ -276,15 +292,16 @@ bool Tokenizer::readDefCount(int &defcount)
         }
         catch (std::out_of_range &e)
         {
-            throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+            if (token.token.at(0) == '-')
+            {
+                throw SyntaxError(NUM_EXPECTED, token.lineNumber, token.lineOffset);
+            }
+            else
+            {
+                throw SyntaxError(TOO_MANY_DEF_IN_MODULE, token.lineNumber, token.lineOffset);
+            }
         }
-
-        // if the number is negative or too big
 
         return true;
     }
 }
-
-// for all the number, must check that it is a number instead of something else, catch the stoi exception!!!!!
-
-// it also might be that the number is just to big, like 213431431431, which exceeds the limitation
